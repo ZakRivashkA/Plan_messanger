dd05c28769de4d6882172a36404c725e8fd49081
Сашка Куропей <vinesauce_super@mail.ru>
2022-11-21 01:33:51 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
+from kivy.uix.label import Label
+
+
+class MainApp(App):
+    def build(self):
+        label = Label(text='Hello',
+                      size_hint=(.5, .5),
+                      pos_hint={'center_x': .5, 'center_y': .5})
+        return label
+
+
+if __name__ == '__main__':
+    app = MainApp()
+    app.run()
